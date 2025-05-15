0c914a3e78e42e8a6a3bb63efef32633aed467c4
Lupastean Iulian - Gabriel <ilupastean@yahoo.com>
2025-05-15 17:14:17 +0300

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -40,8 +40,7 @@ builder.Services.AddCors(options =>
     {
         builder.WithOrigins("http://localhost:3000", "https://nice-water-01e2ed603.6.azurestaticapps.net")
                .AllowAnyHeader()
-               .AllowAnyMethod()
-               .AllowCredentials();
+               .AllowAnyMethod();
     });
 });
 
